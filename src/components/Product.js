7faccd843b13491ecad22b719c57c8e26efda318
aliseslikaya <aliseslikaya@gmail.com>
2022-10-05 17:02:48 +0300

    featured products added

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { formatPrice } from '../utils/helpers'
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-
-const Product = () => {
-  return <h4>product</h4>
-}
-
+import React from "react";
+import styled from "styled-components";
+import { formatPrice } from "../utils/helpers";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Product = ({ image, name, price, id }) => {
+  return (
+    <Wrapper>
+      <div className="container">
+        <img src={image} alt={name} />
+        <Link to={`/products/${id}`} className="link">
+          <FaSearch />
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{formatPrice(price)}</p>
+      </footer>
+    </Wrapper>
+  );
+};
 const Wrapper = styled.article`
   .container {
     position: relative;
@@ -63,5 +74,5 @@ const Wrapper = styled.article`
     color: var(--clr-primary-5);
     letter-spacing: var(--spacing);
   }
-`
-export default Product
+`;
+export default Product;
