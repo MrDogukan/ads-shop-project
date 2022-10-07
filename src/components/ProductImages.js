456e7f26b869b0d52d6eb268edf06e6503a2ccbb
aliseslikaya <aliseslikaya@gmail.com>
2022-10-07 22:42:29 +0300

    image_change

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
-const ProductImages = () => {
-  return <h4>product images</h4>
-}
+const ProductImages = ({ images }) => {
+  const [main, setMain] = useState(images[0]);
+  return <h4>product images</h4>;
+};
 
 const Wrapper = styled.section`
   .main {
@@ -48,6 +49,6 @@ const Wrapper = styled.section`
       }
     }
   }
-`
+`;
 
-export default ProductImages
+export default ProductImages;
