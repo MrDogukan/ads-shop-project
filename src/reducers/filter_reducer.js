a2dfd6e9cff369ec8b59e345bdedb1389efe0ea3
aliseslikaya <aliseslikaya@gmail.com>
2022-10-12 15:21:03 +0300

    gridview added

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -20,6 +20,12 @@ const filter_reducer = (state, action) => {
       filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
   return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
