5c2a447e8830d1ba41a59b01f1e9604f1e0a9a8a
aliseslikaya <aliseslikaya@gmail.com>
2022-10-13 02:03:44 +0300

    sorting added

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,6 +16,7 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
+  sort: "price-lowest",
 };
 
 const FilterContext = React.createContext();
@@ -33,8 +34,17 @@ export const FilterProvider = ({ children }) => {
   const setListView = () => {
     dispatch({ type: SET_LISTVIEW });
   };
+
+  const updateSort = (e) => {
+    // just for demonstration;
+    // const name = e.target.name
+    const value = e.target.value;
+    dispatch({ type: UPDATE_SORT, payload: value });
+  };
   return (
-    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
+    <FilterContext.Provider
+      value={{ ...state, setGridView, setListView, updateSort }}
+    >
       {children}
     </FilterContext.Provider>
   );
