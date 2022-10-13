ca73a768eb42408e40c342c95e8eac7a75200458
aliseslikaya <aliseslikaya@gmail.com>
2022-10-13 14:40:32 +0300

    filetering added

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useFilterContext } from '../context/filter_context'
-import { getUniqueValues, formatPrice } from '../utils/helpers'
-import { FaCheck } from 'react-icons/fa'
+import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import { getUniqueValues, formatPrice } from "../utils/helpers";
+import { FaCheck } from "react-icons/fa";
 
 const Filters = () => {
-  return <h4>filters</h4>
-}
+  const {
+    filters: {
+      text,
+      category,
+      company,
+      color,
+      min_price,
+      price,
+      max_price,
+      shipping,
+    },
+    updateFilters,
+    all_products,
+    clearFilters,
+  } = useFilterContext();
+  return (
+    <Wrapper>
+      <div className="content">
+        <form onSubmit={(e) => e.preventDefault()}>
+          {/* search input */}
+          <div className="form-control">
+            <input
+              type="text"
+              name="text"
+              value={text}
+              placeholder="search"
+              onChange={updateFilters}
+              className="search-input"
+            />
+          </div>
+          {/* end of search input */}
+          {/* category */}
+        </form>
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   .form-control {
@@ -106,6 +141,6 @@ const Wrapper = styled.section`
       top: 1rem;
     }
   }
-`
+`;
 
-export default Filters
+export default Filters;
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -66,6 +66,44 @@ const filter_reducer = (state, action) => {
         return b.name.localeCompare(a.name);
       });
     }
+    if (action.type === UPDATE_FILTERS) {
+      const { name, value } = action.payload;
+      return { ...state, filters: { ...state.filters, [name]: value } };
+    }
+    if (action.type === FILTER_PRODUCTS) {
+      const { all_products } = state;
+      const { text, category, company, color, price, shipping } = state.filters;
+      let tempProducts = [...all_products];
+      if (text) {
+        tempProducts = tempProducts.filter((product) =>
+          product.name.toLowerCase().startsWith(text)
+        );
+      }
+      if (category !== "all") {
+        tempProducts = tempProducts.filter(
+          (product) => product.category === category
+        );
+      }
+      if (company !== "all") {
+        tempProducts = tempProducts.filter(
+          (product) => product.company === company
+        );
+      }
+      if (color !== "all") {
+        tempProducts = tempProducts.filter((product) => {
+          return product.colors.find((c) => c === color);
+        });
+      }
+      // filter by price
+      tempProducts = tempProducts.filter((product) => product.price <= price);
+      // filter by shipping
+      if (shipping) {
+        tempProducts = tempProducts.filter(
+          (product) => product.shipping === true
+        );
+      }
+      return { ...state, filtered_products: tempProducts };
+    }
 
     return { ...state, filtered_products: tempProducts };
   }
