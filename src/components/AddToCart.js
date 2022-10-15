e1d3c46839b9126ae963f8447b31195ace411399
aliseslikaya <aliseslikaya@gmail.com>
2022-10-15 14:16:16 +0300

    cart_reducer added

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,8 +5,7 @@ import { FaCheck } from "react-icons/fa";
 import { useCartContext } from "../context/cart_context";
 import AmountButtons from "./AmountButtons";
 const AddToCart = ({ product }) => {
-  // add to cart
-  // const { addToCart } = useCartContext();
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
@@ -60,7 +59,7 @@ const AddToCart = ({ product }) => {
         <Link
           to="/cart"
           className="btn"
-          // onClick={() => addToCart(id, mainColor, amount, product)}
+          onClick={() => addToCart(id, mainColor, amount, product)}
         >
           add to cart
         </Link>
--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -19,6 +19,12 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  // add to cart
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  };
+
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
