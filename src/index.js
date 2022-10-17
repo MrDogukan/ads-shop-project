3589b7f8caa46b6503095a88c5c79b334000f659
aliseslikaya <aliseslikaya@gmail.com>
2022-10-17 15:34:00 +0300

    auth0 added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+// dev - ycskaf7p.us.auth0.com;
+// 1qP2F5r9VI4ACqIHoTpbaW9IBwyEWAY5
 ReactDOM.render(
   <ProductsProvider>
     <FilterProvider>
